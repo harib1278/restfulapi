7660bddbd8e3560f24ffc922a06bf22571d7951b
harib128 <tomshaddock@googlemail.com>
2020-02-02 16:07:15 +0100

    Setup the migration file for the users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+
 
 class CreateUsersTable extends Migration
 {
@@ -20,7 +22,11 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('verified')->default(User::UNVERFIED_USER);
+            $table->string('verification_token')->nullable();
+            $table->string('admin')->default(User::REGULAR_USER;
             $table->timestamps();
+
         });
     }
 
