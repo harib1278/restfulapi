ada9ed56e1c58885a80a922fe2dd89bba2d187b8
harib128 <tomshaddock@googlemail.com>
2020-02-02 16:34:22 +0100

    Implement a new factory for the user class

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,5 +24,9 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+        'verified' => $verified = $faker->randomElement([User::VERIFIED_USER, User::UNVERIFIED_USER]),
+        'verification_token' => $verified  == User::VERIFIED_USER ? null : User::generateVerificationCode(),
+        'admin' => $verified = $faker->randomElement([User::ADMIN_USER, User::REGULAR_USER]),
+
     ];
 });
