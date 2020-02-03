04a368d02654018969712fc520c8a680e28ab377
harib128 <tomshaddock@googlemail.com>
2020-02-03 18:36:53 +0100

    Fix spelling mistake in the code comments

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // /speify a default string length
+        // /speicfy a default string length
         Schema::defaultStringLength(191);
     }
 }
