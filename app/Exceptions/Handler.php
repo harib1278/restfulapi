c4c471ceb8a240c69057f08571fdb184a8b9a62d
harib128 <tomshaddock@googlemail.com>
2020-02-11 18:08:43 +0100

    Apply some better error handling

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,10 +3,13 @@
 namespace App\Exceptions;
 
 use Exception;
+use App\Traits\ApiResponse;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponse;
     /**
      * A list of the exception types that are not reported.
      *
@@ -50,6 +53,25 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof ValidationException) {
+          return $this->convertValidationExceptionToResponse($exception, $request);
+        }
         return parent::render($request, $exception);
+    }
+
+    /**
+     * Create response object from validation exception
+     *
+     * @param  Illuminate\Validation\ValidationException  $e
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @throws \Exception
+     */
+    protected function convertValidationExceptionToResponse(ValidationException $e, $request)
+    {
+      $errors = $e->validator->errors()->getMessages();
+
+      return $this->errorResponse($errors, 422);
     }
 }
